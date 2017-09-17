bb3258a78c7acdb72b5efbc4a0f30805b91357de
Michael Triantafelow <MTriantafelow@gmail.com>
2017-09-16 22:29:17 -0400

    hello world text on page

--- app/src/main/java/co/thirtyroses/pushups/MainActivity.java
+++ app/src/main/java/co/thirtyroses/pushups/MainActivity.java
@@ -1,13 +1,19 @@
 package co.thirtyroses.pushups;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        String customHtml = "<html><body><h1>Hello, World!</h1></body></html>";
+        webView.loadData(customHtml, "text/html", "UTF-8");
     }
 }
