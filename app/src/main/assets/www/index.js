38f923584651c537351d4a99579cd45755eccc62
Michael Triantafelow <MTriantafelow@gmail.com>
2017-09-20 00:42:00 -0400

    get counter and buttons into center of page

--- app/src/main/assets/www/index.js
+++ app/src/main/assets/www/index.js
@@ -1,6 +1,6 @@
 'use strict';
 (function () {
-let counterSpace = document.getElementById('counter_space');
+let counterSpace = document.getElementById('app_space');
 let counter = document.getElementById('counter');
 counterSpace.onclick = () => {
     counter.textContent = parseInt(counter.textContent) + 1;
