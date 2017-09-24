318ef9310d103936d29082bf408581688886b1d9
Michael Triantafelow <MTriantafelow@gmail.com>
2017-09-24 00:14:37 -0400

    introduced temporary drawer and toolbar

--- app/src/main/assets/www/index.js
+++ app/src/main/assets/www/index.js
@@ -1,6 +1,6 @@
 'use strict';
 (function () {
-let counterSpace = document.getElementById('app_space');
+let counterSpace = document.getElementById('app_main');
 let counter = document.getElementById('counter');
 counterSpace.onclick = () => {
     counter.textContent = parseInt(counter.textContent) + 1;
@@ -9,4 +9,17 @@ let resetButton = document.getElementById("reset_button");
 resetButton.onclick = () => {
     counter.textContent = 0;
 }
+
+var drawerEl = document.querySelector('.mdc-temporary-drawer');
+var MDCTemporaryDrawer = mdc.drawer.MDCTemporaryDrawer;
+var drawer = new MDCTemporaryDrawer(drawerEl);
+document.querySelector('.demo-menu').addEventListener('click', function() {
+  drawer.open = true;
+});
+drawerEl.addEventListener('MDCTemporaryDrawer:open', function() {
+  console.log('Received MDCTemporaryDrawer:open');
+});
+drawerEl.addEventListener('MDCTemporaryDrawer:close', function() {
+  console.log('Received MDCTemporaryDrawer:close');
+});
 })();
