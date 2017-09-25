188680d647a5f215b73bbf7651aa7d8271ad42b0
Michael Triantafelow <MTriantafelow@gmail.com>
2017-09-24 23:36:35 -0400

    only show the counter when "freestyle" is selected

--- app/src/main/assets/www/index.js
+++ app/src/main/assets/www/index.js
@@ -1,25 +1,50 @@
 'use strict';
 (function () {
-let counterSpace = document.getElementById('counter_space');
-let counter = document.getElementById('counter');
-counterSpace.onclick = () => {
-    counter.textContent = parseInt(counter.textContent) + 1;
-}
-let resetButton = document.getElementById("reset_button");
-resetButton.onclick = () => {
-    counter.textContent = 0;
-}
-
 var drawerEl = document.querySelector('.mdc-temporary-drawer');
-var MDCTemporaryDrawer = mdc.drawer.MDCTemporaryDrawer;
-var drawer = new MDCTemporaryDrawer(drawerEl);
-document.querySelector('.demo-menu').addEventListener('click', function() {
+var drawer = new mdc.drawer.MDCTemporaryDrawer(drawerEl);
+document.querySelector('.demo-menu').addEventListener('click', function () {
   drawer.open = true;
 });
-drawerEl.addEventListener('MDCTemporaryDrawer:open', function() {
-  console.log('Received MDCTemporaryDrawer:open');
-});
-drawerEl.addEventListener('MDCTemporaryDrawer:close', function() {
-  console.log('Received MDCTemporaryDrawer:close');
-});
+
+let freestyle = document.getElementById("freestyle");
+freestyle.onclick = () => {
+  CounterActivity.make().show();
+}
+
+// <div id="counter_space">
+//   <span id="counter">0</span>
+// </div>
+const CounterActivity = {
+  make: function() {
+    return {
+      show: function() {
+        var drawerEl = document.querySelector('.mdc-temporary-drawer');
+        var drawer = new mdc.drawer.MDCTemporaryDrawer(drawerEl);
+        drawer.open = false;
+
+        let main = document.getElementById('app_main');
+        let counterSpace = document.createElement('div');
+        counterSpace.id = 'counter_space';
+        let counter = document.createElement('span');
+        counter.id = 'counter';
+        counter.textContent = '0';
+        counterSpace.appendChild(counter);
+        
+        counterSpace.onclick = () => {
+          counter.textContent = parseInt(counter.textContent) + 1;
+        }
+        
+        let resetButton = document.getElementById("reset_button");
+        resetButton.onclick = () => {
+          counter.textContent = 0;
+        }
+
+        while (main.firstChild)
+          main.removeChild(main.firstChild);
+        
+        main.appendChild(counterSpace);
+      }
+    };
+  }
+}
 })();
